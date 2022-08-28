4a52c05b966e33d3f6beba95527f663ded35994a
helloyoogs <95290467+helloyoogs@users.noreply.github.com>
2022-08-29 07:12:52 +0900

    Add files via upload

---
+++ src/components/Detail.vue
@@ -0,0 +1,23 @@
+<template>
+<!-- {{$route.params.id}} 해당 글 아이디 불러올수있다.-->
+  <div>
+    <h5>{{ 블로그글[$route.params.id].title }}</h5>
+    <p>{{ 블로그글[$route.params.id].content }}</p>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Detail',
+  props:{
+    블로그글 : Array,
+  },
+  components: {
+},
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  hi
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
